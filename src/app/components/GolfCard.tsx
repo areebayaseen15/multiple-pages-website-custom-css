468d2066a76efbba77d29da0b042ead1969cf033
Areeba Yaseen <147875287+areebayaseen15@users.noreply.github.com>
2024-11-10 19:27:18 +0500

    Update GolfCard.tsx

--- src/app/components/GolfCard.tsx
+++ src/app/components/GolfCard.tsx
@@ -22,9 +22,7 @@ const GolfCard = () => {
              <h2>INDIVIDUAL LESSONS</h2> 
        <p>Get all the attention you need with a one-to-one lesson.
        Your golf coach for one-off sessions or a series of sessions will ensure you focus on what matters to you and your game in order to reach your goals whether it be performance or results based.</p>
-        <div id='btncard' className='bg-black text-white  w-fit p-3 text-xl font-bold mt-0 ml-2 mb-10 rounded-full '>
-        <Button text={'Contact Our Pros'}/>
-        </div>
+      
       
       </div>
              </div>
